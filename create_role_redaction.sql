94d70986325cfb4891687d51e7804ced9c3d2a4a
mateussmartins <31577875+mateussmartins@users.noreply.github.com>
2017-09-23 17:11:40 -0300

    Add files via upload

--- create_role_redaction.sql
+++ create_role_redaction.sql
@@ -1,4 +1,4 @@
 drop role redaction_users;
 create role redaction_users;
-grant EXEMPT REDACTION POLICY redaction_users;
+grant EXEMPT REDACTION POLICY to redaction_users;
 grant redaction_users to SISVENDA;
